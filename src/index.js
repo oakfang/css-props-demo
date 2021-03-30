307c988030b4bcb7482e2d5e47f2caa7910a8b6d
Alon Niv <alon.niv@taboola.com>
2021-03-30 10:21:00 +0300

    vanilla version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import { ThemeEngine } from "./theme-engine";
 import { ProcessEngine } from "./process-engine";
 import "./styles.css";
 
-ThemeEngine.init();
 ProcessEngine.init();
--- src/process-engine.js
+++ src/process-engine.js
@@ -1,55 +1,57 @@
 export class ProcessEngine {
-    static init() {
-      return new ProcessEngine();
-    }
-  
-    constructor() {
-      this.progressBar = document.querySelector("figure.progress");
-      this.startBtn = document.querySelector("button.start");
-      this.stopBtn = document.querySelector("button.stop");
-      this.setupEvents();
-  
-      this.task = null;
-      this.stop();
-    }
-  
-    setupEvents() {
-      document.querySelectorAll("[data-ctlr]").forEach((btn) => {
-        btn.addEventListener("click", () => this[btn.dataset.ctlr]());
-      });
-    }
-  
-    stop() {
-      document.querySelector("[data-ctlr='start']").disabled = false;
-      document.querySelector("[data-ctlr='stop']").disabled = true;
-      clearInterval(this.task);
-    }
-  
-    start() {
-      document.querySelector("[data-ctlr='start']").disabled = true;
-      document.querySelector("[data-ctlr='stop']").disabled = false;
-      this.task = setInterval(() => {
-        const current = this.getProgress();
-        const next = Math.min(current + 25, 100);
-        this.setProgress(next);
-        if (next === 100) {
-          this.stop();
-        }
-      }, 2000);
-    }
-  
-    clear() {
-      this.stop();
-      this.setProgress(0);
-    }
-  
-    setProgress(progress) {
-      this.progressBar.style.setProperty("--progress", `${progress}%`);
-    }
-  
-    getProgress() {
-      const style = getComputedStyle(this.progressBar);
-      return parseFloat(style.getPropertyValue("--progress"));
-    }
+  static init() {
+    return new ProcessEngine();
   }
-  
+
+  constructor() {
+    this.progressBar = document.querySelector("figure.progress .loaded");
+    this.startBtn = document.querySelector("button.start");
+    this.stopBtn = document.querySelector("button.stop");
+    this.setupEvents();
+
+    this.task = null;
+    this.stop();
+  }
+
+  setupEvents() {
+    document.querySelectorAll("[data-ctlr]").forEach((btn) => {
+      btn.addEventListener("click", () => this[btn.dataset.ctlr]());
+    });
+  }
+
+  stop() {
+    document.querySelector("[data-ctlr='start']").disabled = false;
+    document.querySelector("[data-ctlr='stop']").disabled = true;
+    clearInterval(this.task);
+  }
+
+  start() {
+    document.querySelector("[data-ctlr='start']").disabled = true;
+    document.querySelector("[data-ctlr='stop']").disabled = false;
+    this.task = setInterval(() => {
+      const current = this.getProgress();
+      const next = Math.min(current + 25, 100);
+      this.setProgress(next);
+      if (next === 100) {
+        this.stop();
+      }
+    }, 2000);
+  }
+
+  clear() {
+    this.stop();
+    this.setProgress(0);
+  }
+
+  setProgress(progress) {
+    this.progressBar.style.width = `${progress}%`;
+  }
+
+  getProgress() {
+    const progressWidth = parseFloat(getComputedStyle(this.progressBar).width);
+    const progressContainerWidth = parseFloat(
+      getComputedStyle(this.progressBar.parentElement).width
+    );
+    return (100 * progressWidth) / progressContainerWidth;
+  }
+}
